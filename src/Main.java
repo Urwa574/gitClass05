24adac2700ade7b47c6df7e617b7fa873dd6601f
Urwa574 <urwawaseem574@gmail.com>
2023-03-28 21:57:02 +0500

    My first commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,15 @@ public class Main {
     public static void main(String[] args) {
         for (int i = 0; i < 0; i++) {
             System.out.println(i);
+            //to clone your project
+            //command: git clone URL
+            //make a new project on desktop name it open git bash there check the location by pwd
+            // add command and url
+            //check status or log
+            // to add that clone one to your intellij
+            //open intellij go to file open option than search your project name which is on desktop add them by clicking ok
+            // new window or whatever and here's your clone project
+            //mind this is your own project which you clone
         }
     }
 }
